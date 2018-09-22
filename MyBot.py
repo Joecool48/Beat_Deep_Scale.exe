8e15f1a2612071db19a17ffa9e150e94f71bb0a3
Joecool48 <30698327+Joecool48@users.noreply.github.com>
2018-09-22 16:42:44 -0500

    Finished code for attacking algorithm

--- MyBot.py
+++ MyBot.py
@@ -19,6 +19,70 @@ PLAYER_PAPER_REWARD_WEIGHT = 20
 PLAYER_SCISSORS_REWARD_WEIGHT = 20
 PLAYER_SPEED_REWARD_WEIGHT = 20
 PLAYER_HEALTH_REWARD_WEIGHT = 30
+
+def monster_score (monster):
+    score = 0
+    score -= monster.health * MONSTER_HEALTH_WEIGHT
+    score -= monster.attack * MONSTER_ATTACK_WEIGHT
+    score += monster.death_effects.rock * PLAYER_ROCK_REWARD_WEIGHT
+    score += monster.death_effects.paper * PLAYER_PAPER_REWARD_WEIGHT
+    score += monster.death_effects.scissors * PLAYER_SCISSORS_REWARD_WEIGHT
+    score += monster.death_effects.health * PLAYER_HEALTH_REWARD_WEIGHT
+    score += monster.death_effects.speed * PLAYER_SPEED_REWARD_WEIGHT
+    return score
+def max_score_monster(nodes):
+    if len(nodes) == 0:
+        return None
+    max_score = -10000000
+    max_monster = None
+    for node in nodes:
+        if game.has_monster(node) and game.get_self().location != node and game.get_monster(node).health > 0:
+            monster = game.get_monster(node)
+            score = monster_score(monster)
+            if score > max_score and node != 0:
+                max_score = score
+                max_monster = monster
+
+    return max_monster
+
+# Healing monster spawns at turn 40
+def attack_decision(node):
+    chosen_stance = stances[random.randint(0,2)]
+    destination_node = game.get_self().location
+    max_score = -1000000
+    max_location = 0
+    if game.get_self().health < AUTO_RETREAT_HEALTH and game.get_monster(0).health > 0:
+        game.log("Retreat")
+        shortest = game.shortest_paths(game.get_self().location, 0)
+        max_location = shortest[0][0]
+        for path in shortest:
+            score = monster_score(game.get_monster(path[0]))
+            if not game.has_monster(path[0]):
+                destination_node = path[0]
+            elif score > max_score:
+                max_score = score
+                max_location = path[0]
+        destination_node = max_location
+        game.log("Dest node = " + str(destination_node))
+    elif game.has_monster(game.get_self().location) and game.get_monster(game.get_self().location).health > 0:
+        game.log("Current node has monster")
+        chosen_stance = get_winning_stance(game.get_monster(game.get_self().location).stance)
+    else:
+        game.log("No retreat or monster on space")
+        nodes = game.get_adjacent_nodes(game.get_self().location)
+        monst = max_score_monster(nodes)
+        if monst != None:
+            destination_node = monst.location
+            game.log("Dest node = " + str(destination_node))
+            chosen_stance = get_winning_stance(monst.stance)
+        else:
+            game.log("choose a node")
+            all_nodes = game.get_all_monsters()
+            if len(all_nodes) == 0:
+                destination_node = 0
+            destination_node = all_nodes[0].location
+            game.log("Dest node = " + str(destination_node))
+    return (chosen_stance, destination_node)
 def get_winning_stance(stance):
     if stance == "Rock":
         return "Paper"
@@ -37,85 +101,5 @@ for line in fileinput.input():
     game.update(json.loads(line))
 # DO NOT CHANGE ABOVE ---------------------------
 
-def monster_score (monster):
-    score = 0
-    score -= monster.health * MONSTER_HEALTH_WEIGHT
-    score -= monster.attack * MONSTER_ATTACK_WEIGHT
-    score += monster.death_effects.rock * PLAYER_ROCK_REWARD_WEIGHT
-    score += monster.death_effects.paper * PLAYER_PAPER_REWARD_WEIGHT
-    score += monster.death_effects.scissors * PLAYER_SCISSORS_REWARD_WEIGHT
-    score += monster.death_effects.health * PLAYER_HEALTH_REWARD_WEIGHT
-    score += monster.death_effects.speed * PLAYER_SPEED_REWARD_WEIGHT
-    return score
-def max_score_monster(nodes):
-    if len(nodes) == 0:
-        return None
-    max_score = -10000000
-    max_monster = None
-    for node in nodes:
-        if game.has_monster(node):
-            monster = game.get_monster(node)
-            score = monster_score(monster)
-            if score > max_score:
-                max_score = score
-                max_monster = monster
-
-    return max_monster
-
-# Healing monster spawns at turn 40
-def attack_decision(node):
-    if len(nodes) == 0:
-        return
-    chosen_stance = stances[random.randint(0,2)]
-    destination_node = game.get_self().location
-    max_score = -1000000
-    max_location = 0
-    if game.get_self().health < AUTO_RETREAT_HEALTH:
-        shortest = game.shortest_paths(game.get_self().location, 0)
-        max_location = shortest[0][0]
-        for path in shortest:
-            score = monster_score(game.get_monster(path[0]))
-            if not game.has_monster(path[0]):
-                destination_node = path[0]
-            elif score > max_score:
-                max_score = score
-                max_location = path[0]
-        destination_node = max_location
-    elif game.has_monster(game.get_self().location):
-        chosen_stance = get_winning_stance(game.get_monster(game.get_self().location).stance)
-    else:
-        nodes = game.get_adjacent_nodes(game.get_self().location)
-        monst = max_score_monster(nodes)
-        if monst != None:
-            destination_node = monst.location
-            chosen_stance = get_winning_stance(monst.stance)
-        else:
-            destination_node = nodes[random.randint(0, len(nodes) - 1)]
-
-    return (chosen_stance, destination_node)
-
-    # me = game.get_self()
-    #
-    # if me.location == me.destination: # check if we have moved this turn
-    #     # get all living monsters closest to me
-    #     monsters = game.nearest_monsters(me.location, 1)
-    #
-    #     # choose a monster to move to at random
-    #     monster_to_move_to = monsters[random.randint(0, len(monsters)-1)]
-    #
-    #     # get the set of shortest paths to that monster
-    #     paths = game.shortest_paths(me.location, monster_to_move_to.location)
-    #     destination_node = paths[random.randint(0, len(paths)-1)][0]
-    # else:
-    #     destination_node = me.destination
-    #
-    # if game.has_monster(me.location):
-    #     # if there's a monster at my location, choose the stance that damages that monster
-    #     chosen_stance = get_winning_stance(game.get_monster(me.location).stance)
-    # else:
-    #     # otherwise, pick a random stance
-    #     chosen_stance = stances[random.randint(0, 2)]
-    #
-    # # submit your decision for the turn (This function should be called exactly once per turn)
     decision = attack_decision(game.get_self().location)
-    game.submit_decision(decision[0], decision[1])
+    game.submit_decision(decision[1], decision[0])
