216e41bf03c08769593afe1882ddadd395e78619
Brian Furman <bfurman777@yahoo.com>
2018-09-22 15:50:40 -0500

    +DummyBot

--- dummy-bot/MyBot.py
+++ dummy-bot/MyBot.py
@@ -10,8 +10,9 @@ first_line = True # DO NOT REMOVE
 
 # global variables or other functions can go here
 stances = ["Rock", "Paper", "Scissors"]
-loop_locations = [0, 1, 3, 1]
+loop_number = 0
 loop_index = 0;
+loop_list = [[0, 10, 9, 8, 14],[19]]
 
 def get_winning_stance(stance):
     if stance == "Rock":
@@ -35,18 +36,23 @@ for line in fileinput.input():
 
     me = game.get_self()
 
-    if game.get_monster(me.location).health > 0:
+    if game.has_monster and game.get_monster(me.location).health > 0:
         # if there's a living monster at my location, choose the stance that damages that monster, don't change location
         chosen_stance = get_winning_stance(game.get_monster(me.location).stance)
     else:
         # otherwise, go to the next location in the loop
-        if (loop_locations[loop_index] == me.location):    #only change my target if I moved to the previous target
-            loop_index = (loop_index + 1) % 4
+        if (loop_list[loop_number][loop_index] == me.location):    #only change my target if I moved to the previous target
+            loop_index = loop_index + 1
+        if (loop_index > len(loop_list[loop_number]) - 1):
+            loop_index = 0
+            loop_number = loop_number + 1
         chosen_stance = stances[random.randint(0, 2)]
+    if (loop_number > len(loop_list) - 1):
+        loop_number = len(loop_list) - 1
 
-    destination_node = loop_locations[loop_index];
+    destination_node = loop_list[loop_number][loop_index];
 
-    game.log("Turn: {0},\tMyNode: {1},\tHasMonster: {2},\tMonsterHealth: {3},\tDestinationNode: {4},\tloopIndex: {5}".format(game.turn_number, me.location, game.has_monster(me.location), game.get_monster(9).health, destination_node, loop_index))
+    #game.log("Turn: {0},\tMyNode: {1},\tloopNumber: {2},\tMonsterHealth: {3},\tDestinationNode: {4},\tloopIndex: {5}".format(game.turn_number, me.location, loop_number, game.get_monster(9).health, destination_node, loop_index))
 
 
     # submit your decision for the turn (This function should be called exactly once per turn)
